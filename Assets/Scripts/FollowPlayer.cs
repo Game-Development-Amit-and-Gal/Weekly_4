0d156152d0b99f4a0f925d348260de4471b545a5
AmitNachum <amitn0@hotmail.co.il>
2025-11-20 20:32:48 +0200

    SpaceShip fall

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+[RequireComponent (typeof(Transform))]
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Vector3 offset;
+
+
+    void LateUpdate()
+    {
+        Vector3 desiredPos = playerTransform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPos, 0.1f);
+        transform.LookAt(playerTransform);
+    }
+
+}
